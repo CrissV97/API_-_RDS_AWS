3d4d00bd846a6f7db1406ec81310f061b5dc1143
CrissV97 <ariel_cris1999@hotmail.com>
2026-09-24 21:00:57 -0500

    Change default DATABASE_URL for PostgreSQL

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mi_base_datos"
+    DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/fastapidb"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
